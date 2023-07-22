96ffaab275215b57c42cc634e9b030e8da9a250a
vladsendrix <vladsendrix@gmail.com>
2023-07-22 18:11:01 +0300

    realign items to center

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
 	import type { Writable } from 'svelte/store'
 
 	interface Contacts {
-			name: string
-			phone: string
-		}
+		name: string
+		phone: string
+	}
 
 	const contactStore: Writable<Contacts[]> = localStorageStore('contacts', [])
 
@@ -13,7 +13,7 @@
 	let inputPhone = ''
 
 	function addContact() {
-		$contactStore = [ { name: inputName, phone: inputPhone }, ...$contactStore]
+		$contactStore = [{ name: inputName, phone: inputPhone }, ...$contactStore]
 		inputName = ''
 		inputPhone = ''
 	}
@@ -24,35 +24,38 @@
 			return contacts
 		})
 	}
-
 </script>
 
 <div class="container mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1 m-10">Contacts</h1>
-		<p>A storage persitent contacts store page</p>
+		<h1 class="h1 m-10 text-center">Contacts</h1>
+		<p class="text-center">A storage persitent contacts store page</p>
 		<label class="label">
-			<span class="label-text">Name</span>
 			<input class="input" type="text" placeholder="Name" bind:value={inputName} />
 		</label>
 		<label class="label">
-			<span class="label-text">Phone Number</span>
 			<input class="input" type="text" placeholder="Phone Number" bind:value={inputPhone} />
 		</label>
-		<button type="button" class="btn variant-filled" on:click={addContact}>Add contact</button>
+		<div class="flex justify-center">
+			<button type="button" class="btn variant-filled" on:click={addContact}>Add contact</button>
+		</div>
 		<hr />
-		<h2 class="h2">Your contacts:</h2>
+		<h2 class="h2 text-center">Your contacts:</h2>
 		{#each $contactStore as contact, index}
-			<div class="card">
+			<div class="card flex justify-between place-items-center">
 				<div class="card-body p-4 text-lg">
 					<p class="card-title font-bold">{contact.name}</p>
 					<p class="card-subtitle">{contact.phone}</p>
-					<button type="button" class="btn btn-sm variant-filled" on:click={() => deleteContact(index)}>Delete</button>
 				</div>
+				<button
+					type="button"
+					class="btn btn-lg variant-filled m-2 p-6 h-6"
+					on:click={() => deleteContact(index)}>Delete</button
+				>
 			</div>
 		{/each}
 		{#if $contactStore.length === 0}
-			<p>You don't have any contacts yet</p>
+			<p class="text-center">You don't have any contacts yet</p>
 		{/if}
 	</div>
 </div>
