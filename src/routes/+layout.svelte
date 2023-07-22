66006a1df05524d59622e3deedc52eb2e02da6f4
vladsendrix <vladsendrix@gmail.com>
2023-07-22 19:12:51 +0300

    set up light/dark mode based on device theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,9 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+
+	import { autoModeWatcher } from '@skeletonlabs/skeleton';
 </script>
 
+<svelte:head>{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}</svelte:head>
 <slot />
